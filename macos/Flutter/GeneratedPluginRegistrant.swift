dc8fa89aceeafb2b7687b00e362f2dc02c48c06e
PRANTA Dutta <pranta.dutta@kdsgroup.net>
2026-08-07 12:57:44 +0600

    Drop Firebase Remote Config
    Remote Config is a paid service now and Odyssey used it for exactly one
    thing: overriding the ad cadence knobs in AdConstants at startup. Not
    worth a bill.
    
    Ad intensity is compile-time from here on. The knobs stay in one file,
    but changing them means shipping a release rather than editing a console
    value.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,6 @@ import firebase_auth
 import firebase_core
 import firebase_crashlytics
 import firebase_messaging
-import firebase_remote_config
 import flutter_local_notifications
 import flutter_secure_storage_darwin
 import geolocator_apple
@@ -37,7 +36,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
